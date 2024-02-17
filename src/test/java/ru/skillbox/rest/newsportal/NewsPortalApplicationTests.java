a595573d71d403211c491dbaf13a7322c12959de
Nicko404 <kolsak2013@yandex.ru>
2024-02-17 13:08:29 +0300

    Initial

---
+++ src/test/java/ru/skillbox/rest/newsportal/NewsPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.skillbox.rest.newsportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsPortalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
